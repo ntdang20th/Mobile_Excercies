a74a92df96fe4400bbc5ece4e4f149a3c183f973
Dang <ntdang_20th@student.agu.edu.vn>
2022-10-01 17:01:24 +0700

    first commit

---
+++ bt12_alert_displaypopup/bt12_alert_displaypopup/bt12_alert_displaypopup/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace bt12_alert_displaypopup
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Username.Text = Preferences.Get("user", "");
+            password.Text = Preferences.Get("pass", "");
+            ckbRemember.IsChecked = Preferences.ContainsKey("pass");
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            string user = Username.Text.Trim();
+            string pass = password.Text.Trim();
+            bool ischecked = ckbRemember.IsChecked;
+            bool islogin = user == pass ? true : false;
+            if (islogin)
+                if (ischecked)
+                {
+                    DisplayAlert("Thông báo đăng nhập", "Đăng nhập thành công! Nhớ mật khẩu.", "OK");
+                    Preferences.Set("user", user);
+                    Preferences.Set("pass", pass);
+                }
+                else
+                {
+                    DisplayAlert("Thông báo đăng nhập", "Đăng nhập thành công! Không lưu mật khẩu.", "OK");
+                    Preferences.Clear();
+                }
+            else
+                DisplayAlert("Thông báo đăng nhập", "Đăng nhập thất bại. Vui lòng kiểm tra tên đăng nhập hoặc mật khẩu.", "OK");
+        }
+    }
+}
---
+++ bt8_convert/bt8_convert/bt8_convert/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace bt8_convert
+{
+    public partial class MainPage : ContentPage
+    {
+        private double _cel;
+        private double _fah;
+
+        public double Cel { get => _cel; set => _cel = value; }
+        public double Fah { get => _fah; set => _fah = value; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = this;
+            
+        }
+
+        private void btnToCel_Clicked(object sender, EventArgs e)
+        {
+            Cel = (Fah - 32) * 5 / 9;
+            entryCel.Text = String.Format("{0}°C", Math.Round(Cel, 2));
+            entryFah.Text = String.Format("{0}°F", Math.Round(Fah, 2));
+        }
+
+        private void btnToFah_Clicked(object sender, EventArgs e)
+        {
+            Fah = Cel * 9 / 5 +32;
+            entryCel.Text = String.Format("{0}°C", Cel);
+            entryFah.Text = String.Format("{0}°F", Fah);
+        }
+
+        private void btnClear_Clicked(object sender, EventArgs e)
+        {
+            entryCel.Text = entryFah.Text = String.Empty;
+        }
+
+        private void entryCel_Focused(object sender, FocusEventArgs e)
+        {
+            entryCel.Text = "";
+        }
+
+        private void entryFah_Focused(object sender, FocusEventArgs e)
+        {
+            entryFah.Text = "";
+        }
+
+    }
+}
